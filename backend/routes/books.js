9e3e520dae2a1e76e78674468ee824752478432e
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-20 04:42:03 -0500

    Added Express route to post a new book

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -10,3 +10,14 @@ router.get('/', async (req, res) => {
     res.json(books);
 });
 
+// post a new book
+router.post('/', async (req, res) => {
+    const newBook = new Book(req.body);
+    const existingBook = await Book.findOne({ id: newBook.id });
+    if (existingBook) {
+        return res.status(400).json({ message: 'Book already exists' });
+    } else {
+        await newBook.save();
+        res.status(201).json(newBook);
+    }
+});
