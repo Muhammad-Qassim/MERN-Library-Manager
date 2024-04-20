76a34cdce551ddb9702624aa6f44befc6b868baf
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-20 05:28:06 -0500

    Connected to MongoDB using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-//Cors middleware
+//CORS middleware
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
@@ -13,3 +13,11 @@ app.use(function(req, res, next){
     if (req.method === "OPTIONS") res.sendStatus(200);
     else next();
   });
+
+//connect to MongoDB
+const mongoCS = require('dotenv').config();
+mongoose.connect(mongoCS, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "MongoDB connection error:"));
+connection.once("open", function() { console.log("connected");  });
