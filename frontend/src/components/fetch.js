94e3b1a5c3bc7d45eda931ec17434a470ad17323
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-21 00:25:32 -0500

    Created separate API functions in fetch.js for fetching books

---
+++ frontend/src/components/fetch.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const fetchBooks = () => {
+  return axios.get('http://localhost:3000/books?status=true')
+    .then(res => res.data)
+    .catch(err => console.log(err));
+};
+
+export const fetchCheckedOutBooks = () => {
+  return axios.get('http://localhost:3000/books/checkedout?status=false')
+    .then(res => res.data)
+    .catch(err => console.log(err));
+};
