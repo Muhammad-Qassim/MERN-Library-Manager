81e77aa8b588d030383c0fbdb6808439f1c0557c
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-20 22:50:37 -0500

    Created Navbar & Footer and initialize bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function Nav() {
+  return (
+    <nav className = "navbar navbar-dark bg-dark pt-3 pb-3">
+      <div className="container">
+          <a className="navbar-brand mx-auto" href="#">MERN Library Management</a>
+        </div>
+    </nav>
+);
+}
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white pt-3 pb-3 mt-5">
+      <div className="container text-center">
+      <p className='m-0'>&copy;2024 Muhammad Qassim. All rights reserved.</p>
+    </div>
+  </footer>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <div>
+        <h1>BODY</h1>
+      </div>
+      <Footer/>
     </div>
+      
   );
 }
 
